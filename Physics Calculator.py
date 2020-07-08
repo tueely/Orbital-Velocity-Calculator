2ad7c36cbc28dcc8df39fd9f2cb58028f6bc08da
tueely <67970162+tueely@users.noreply.github.com>
2020-07-08 13:28:43 +0300

    Add files via upload
    The powerpoint data values could be slightly off the real values as technology is increasing and the readings are as well, although, it is recommended that the user uses the internet so that they can get better and more correct values as a result.

---
+++ Physics Calculator.py
@@ -0,0 +1,31 @@
+print ('Calculator') 
+print ('Coded by Ayush')
+print ('It is Highly Possible that the digits will be one or two figures off the real value because some of the data in the PowerPoint and the codes is rounded-off.')
+
+A = int(input('Enter the diameter (km) (look at the powerpoint): '))
+
+B = A*0.5*1000
+C = (B**3 * 4/3 * 3.1415926535)
+
+D = int(input('Enter the density (look at the powerpoint): '))
+
+print('Mass of the Planet:') 
+print (C*D)
+E = C*D
+
+F = int(input('Enter the distance from the Sun (in millions) (look at the powerpoint): '))\
+
+G = F*10**9
+
+print ('your answer is')\
+# print octal value 
+
+print(E*1.989*10**30*6.67*10**-11/G**2)\
+
+H = E*1.989*10**30*6.67*10**-11/G**2\
+
+value = input("What is the name of the Planet?:\n")\
+
+print(f'Hence, {value} has a {H}N Gravitational Force from the Sun! ')\
+
+print (f'Thanks for using this Gravitational Attraction Calculator!')
